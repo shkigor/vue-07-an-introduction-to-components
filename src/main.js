d1847dbe14010cfcf52c868efb83697f6aa76c4b
Ihor Shkliar <igor.shklyar@gmail.com>
2020-05-15 23:40:14 +0300

    98. Creating a Component. And registry it globally.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from "./components/Home";
 
 Vue.config.productionTip = false
+
+Vue.component('app-server-status', Home);
 
 new Vue({
   render: h => h(App),
