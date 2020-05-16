ddfcf3c6b3120c213778744e8dc16bb4aa40f405
Ihor Shkliar <igor.shklyar@gmail.com>
2020-05-16 05:40:13 +0300

    100. Time to Practice - Components (Solution)

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,30 @@
 <template>
-  <div id="app" class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <header>
-          <h1>Server Status</h1>
-        </header>
-      </div>
+    <div id="app" class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
     </div>
-    <hr>
-    <div class="row">
-      <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-          <li
-                  class="list-group-item"
-                  v-for="index in 5"
-                  v-bind:key="index">
-            Server #{{ index }}
-          </li>
-        </ul>
-      </div>
-      <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
-      </div>
-    </div>
-    <hr>
-    <div class="row">
-      <div class="col-xs-12">
-        <footer>
-          <p>All Servers are managed here</p>
-        </footer>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
-  export default {
+    import Header from "./components/Header";
+    import Footer from "./components/Footer";
+    import Servers from "./components/Servers";
+    import ServerDetails from "./components/ServerDetails";
 
-  }
+    export default {
+        components: {
+            'app-header': Header,
+            'app-footer': Footer,
+            'app-servers': Servers,
+            'app-server-details': ServerDetails
+        }
+    }
 </script>
 
 <style>
