8d8eae49d20d4c2d5ee0fc9ece6e672264d3165c
Ihor Shkliar <igor.shklyar@gmail.com>
2020-05-19 22:15:51 +0300

    119. Time to Practice - Component Communication (Code)

--- src/components/Server/Servers.vue
+++ src/components/Server/Servers.vue
@@ -1,19 +1,31 @@
 <template>
     <div class="col-xs-12 col-sm-6">
         <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5"
-                    v-bind:key="index">
-                Server #{{ index }}
-            </li>
+            <app-server v-for="server in servers"
+                    :key="server.id"
+                    :server="server"
+            ></app-server>
         </ul>
     </div>
 </template>
 
 <script>
+    import Server from './Server.vue';
+
     export default {
-        name: "Servers"
+        data: function () {
+            return {
+                servers: [
+                    {id: 1, status: 'Normal'},
+                    {id: 2, status: 'Critical'},
+                    {id: 3, status: 'Unknown'},
+                    {id: 4, status: 'Normal'}
+                ]
+            }
+        },
+        components: {
+            appServer: Server
+        }
     }
 </script>
 
